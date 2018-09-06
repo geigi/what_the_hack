34fe3ac6100b418109d5d997a59e51937761d98f
Julian Geywitz <github@geigi.de>
2018-09-06 17:09:41 +0200

    Separate Editor DLL

---
+++ Assets/Scripts/Mods/ModLoader.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ModTool;
+using UnityEngine;
+using UnityEngine.UI;
+using Wth.ModApi;
+
+public class ModLoader : MonoBehaviour {
+	public Text titleText, descriptionText;
+	public Image banner;
+	public GameObject loadingPanel;
+
+	private Mod currentMod;
+	private List<Mod> mods;
+	private ModManager modManager;
+	private string baseTitle, baseDescription;
+	private Sprite baseBanner;
+
+	// Use this for initialization
+	void Start () {
+		baseTitle = titleText.text;
+		baseDescription = descriptionText.text;
+		baseBanner = banner.sprite;
+
+		mods = new List<Mod>();
+		modManager = ModManager.instance;
+
+		modManager.refreshInterval = 15;
+
+        foreach (Mod mod in modManager.mods)
+            OnModFound(mod);
+
+        modManager.ModFound += OnModFound;
+        modManager.ModRemoved += OnModRemoved;
+		modManager.ModLoaded += OnModLoaded;
+	}
+
+	public void NextMod() {
+		if (currentMod != null) {
+			var currentModIndex = mods.FindIndex(p => p == currentMod);
+			if (currentModIndex < mods.Count - 1) {
+				currentMod.Unload();
+				currentMod = mods[currentModIndex + 1];
+				LoadMod(currentMod);
+			}
+			else {
+				return;
+			}
+		}
+		else {
+			if (mods.Count > 0) {
+				currentMod = mods[0];
+				LoadMod(currentMod);
+			}
+			else {
+				// No mods found.
+				return;
+			}
+		}
+	}
+
+	public void PrevMod() {
+		if (currentMod == null) {
+			// We are at the beginning of the List, no more going further.
+			return;
+		}
+		else {
+			var currentModIndex = mods.FindIndex(p => p == currentMod);
+			if (currentModIndex < 1) {
+				currentMod.Unload();
+				currentMod = null;
+				DisplayBaseGame();
+			}
+			else {
+				currentMod.Unload();
+				currentMod = mods[currentModIndex - 1];
+				LoadMod(currentMod);
+			}
+		}
+	}
+
+	private bool ValidateMod(Mod mod) {
+		try {
+			mod.GetAsset("ModInfo");
+			return true;
+		}
+		catch (Exception e) {
+			return false;
+		}
+		
+	}
+
+	private void LoadMod(Mod mod) {
+		loadingPanel.SetActive(true);
+		mod.LoadAsync();
+	}
+
+	private void DisplayBaseGame() {
+		titleText.text = baseTitle;
+		descriptionText.text = baseDescription;
+		banner.sprite = baseBanner;
+	}
+
+	private void DisplayMod(Mod mod) {
+		titleText.text = mod.modInfo.name;
+		descriptionText.text = mod.modInfo.description;
+		banner.sprite = mod.GetAsset<ModInfo>("modinfo").banner;
+	}
+
+	private void OnModFound(Mod mod)
+    {
+		if (ValidateMod(mod)) {
+			mods.Add(mod);
+		}
+		else {
+			Debug.LogWarning("Could not load mod:");
+			LogModInfo(mod);
+		}
+    }
+
+    private void OnModRemoved(Mod mod)
+    {
+        mods.Remove(mod);
+    }
+
+	private void OnModLoaded(Mod mod) {
+		loadingPanel.SetActive(false);
+		DisplayMod(mod);
+	}
+
+	private void LogModInfo(Mod mod) {
+		Debug.Log(mod.modInfo);
+		Debug.Log("AssemblyNames:");
+		Debug.Log(mod.assemblyNames);
+		Debug.Log("AssetPaths:");
+		Debug.Log(mod.assetPaths);
+	}
+}
---
+++ Wth.ModApi/Wth.ModApi.Editor/Employees/CreateEmployeeList.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;                                                                    
+using System.Collections;
+using UnityEditor;
+using Wth.ModApi;
+
+namespace Wth.ModApi.Editor {
+public class CreateEmployeeList
+{
+    [MenuItem("Assets/Create/Employee List")]
+    public static EmployeeList Create()
+    {
+        EmployeeList asset = ScriptableObject.CreateInstance<EmployeeList>();
+
+        AssetDatabase.CreateAsset(asset, "Assets/EmployeeList.asset");
+        AssetDatabase.SaveAssets();
+        return asset;
+    }
+}
+}
