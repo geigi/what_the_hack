3bc1bab4f8e284444d9af3ab186db95095890c71
Julian Geywitz <github@geigi.de>
2018-09-06 13:49:26 +0200

    Implemented basic ModApi
    ModApi is now a separate C# solution which is compiled separately and included in Unity as DLL.
    Menu scene already searches and loads mods and displays them.

---
+++ Wth.ModApi/Wth.ModApi/ModInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wth.ModApi {
+    [CreateAssetMenu(fileName = "ModInfo", menuName = "Mod/ModInfo", order = 1)]
+    public class ModInfo : ScriptableObject {
+        public Sprite banner;
+	
+    }
+}
