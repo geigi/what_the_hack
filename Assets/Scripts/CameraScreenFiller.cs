d2ddb4d8c2c89fe57a92aaa22f33ff5fdc5fe435
Julian Geywitz <github@geigi.de>
2018-09-10 17:34:56 +0200

    Implemented pixel perfect camera for main scene

--- Assets/Scripts/CameraScreenFiller.cs
+++ Assets/Scripts/CameraScreenFiller.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class CameraScreenFiller : MonoBehaviour {
 	public Camera mainCamera;
+	public PixelPerfectCamera pixelPerfectCamera;
 	public SpriteRenderer background;
 
 	// Use this for initialization
 	void Start () {
-		//Fill ultra widescreen devices
-		float screenAspect = (float) Screen.width / (float) Screen.height;
-		if (screenAspect > 1.7777f) {
-			var width = background.sprite.bounds.size.x;
-			var height = width / screenAspect;
-			mainCamera.orthographicSize = height / 2;
+		Debug.Log(mainCamera.orthographicSize);
+		if (mainCamera.orthographicSize < 3.3f) {
+			pixelPerfectCamera.enabled = false;
+
+			//Fill ultra widescreen devices
+			float screenAspect = (float) Screen.width / (float) Screen.height;
+			if (screenAspect > 1.7777f) {
+				var width = background.sprite.bounds.size.x;
+				var height = width / screenAspect;
+				mainCamera.orthographicSize = height / 2;
+			}
 		}
 	}
 }
