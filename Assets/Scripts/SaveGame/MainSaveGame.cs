9eb5aae344fe72d29be5986beb3a33668a0ba615
Julian Geywitz <github@geigi.de>
2018-09-20 12:11:11 +0200

    Added basic saveGame properties

--- Assets/Scripts/SaveGame/MainSaveGame.cs
+++ Assets/Scripts/SaveGame/MainSaveGame.cs
@@ -2,8 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Wth.ModApi;
 
 [Serializable]
-public class MainSaveGame : SaveGame {
+public class MainSaveGame : SaveGame
+{
+	public string name { get; set; }
+	public string modId { get; set; }
+	public DateTime saveDate { get; set; }
 	
+	public List<EmployeeData> employeesForHire { get; set; }
+	public List<EmployeeData> employeesHired { get; set; }
 }
