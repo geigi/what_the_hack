53a6c763f15615b5f1f5718e58085bf856fb8c14
Julian Geywitz <github@geigi.de>
2018-11-12 14:02:29 +0100

    Created separate Assembly Definition to make testing work

--- Assets/Tests/Wth Tests.cs
+++ Assets/Tests/Wth Tests.cs
@@ -2,9 +2,17 @@
 using UnityEngine.TestTools;
 using NUnit.Framework;
 using System.Collections;
+using UnityEditor.SceneManagement;
+using Wth.ModApi.Employees;
 
 public class WthModApiTests {
 
+    [SetUp]
+    public void ResetScene()
+    {
+        EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
+    }
+    
     [Test]
     public void WthModApiTestsSimplePasses() {
         // Use the Assert class to test conditions.
@@ -17,5 +25,13 @@ public class WthModApiTests {
         // Use the Assert class to test conditions.
         // yield to skip a frame
         yield return null;
+    }
+    
+    [Test]
+    public void BasicTest()
+    {
+        GameObject go = new GameObject();
+        go.AddComponent<Employee>();
+        Assert.IsNotNull(go.GetComponent<Employee>());
     }
 }
