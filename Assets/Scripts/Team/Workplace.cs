2220e939ebe993ca623850348518070d09c813e2
Julian Geywitz <github@geigi.de>
2019-01-30 17:39:09 +0100

    Workplaces now take up 3 tiles

--- Assets/Scripts/Team/Workplace.cs
+++ Assets/Scripts/Team/Workplace.cs
@@ -18,18 +18,16 @@ namespace Team
         public SpriteRenderer Desk;
         public SpriteRenderer Pc;
         public SpriteRenderer Chair;
-
-        private Vector2Int Position;
+        public Vector2Int Position;
+        
         private bool testTiles = false;
         private Vector2Int position2;
+        private Vector2Int position3;
 
         private void Start()
         {
-            var pos = transform.position + new Vector3(-0.2f, -0.2f, 0);
-            var cellPos = Grid.go_grid.WorldToCell(pos);
-            Position = new Vector2Int(cellPos.x, cellPos.y);
-            Debug.Log(Position);
-            var layer = Grid.CalculateSortingLayer(pos);
+            var BottomTilePosition = Grid.go_grid.CellToWorld(new Vector3Int(Position.x, Position.y + 1, 0));
+            var layer = Grid.CalculateSortingLayer(BottomTilePosition);
             Desk.sortingOrder = layer;
             Pc.sortingOrder = layer + 1;
             Chair.sortingOrder = layer - 1;
@@ -41,6 +39,7 @@ namespace Team
             if (testTiles)
             {
                 if (Grid.GetNodeState(Position) == Enums.TileState.FREE &&
+                    Grid.GetNodeState(position2) == Enums.TileState.FREE && 
                     Grid.GetNodeState(position2) == Enums.TileState.FREE)
                 {
                     testTiles = false;
@@ -59,11 +58,13 @@ namespace Team
             {
                 // A Workplace blocks 2 tiles
                 position2 = new Vector2Int(Position.x, Position.y - 1);
+                position3 = new Vector2Int(Position.x, Position.y + 1);
                 
                 // Test if tiles are blocked/occupied
                 var node1State = Grid.GetNodeState(Position);
                 var node2State = Grid.GetNodeState(position2);
-                if (node1State != Enums.TileState.FREE || node2State != Enums.TileState.FREE)
+                var node3State = Grid.GetNodeState(position3);
+                if (node1State != Enums.TileState.FREE || node2State != Enums.TileState.FREE || node3State != Enums.TileState.FREE)
                 {
                     // If they are occupied we test each frame if the tile is now free
                     gameObject.SetActive(true);
@@ -90,6 +91,7 @@ namespace Team
                 Pc.enabled = true;
                 Grid.SetNodeState(Position, Enums.TileState.BLOCKED);
                 Grid.SetNodeState(position2, Enums.TileState.BLOCKED);
+                Grid.SetNodeState(position3, Enums.TileState.BLOCKED);
             }
             else
             {
@@ -99,6 +101,7 @@ namespace Team
                 Pc.enabled = false;
                 Grid.SetNodeState(Position, Enums.TileState.FREE);
                 Grid.SetNodeState(position2, Enums.TileState.FREE);
+                Grid.SetNodeState(position3, Enums.TileState.FREE);
             }
         }
 
