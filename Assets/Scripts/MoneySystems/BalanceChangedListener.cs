ef340413eb9ebde7ae58f30a85e343ec4615b99a
Tobias-Melzer <tobias.melzer@study.hs-duesseldorf.de>
2019-03-06 15:52:19 +0100

    Builds should now work

--- Assets/Scripts/MoneySystems/BalanceChangedListener.cs
+++ Assets/Scripts/MoneySystems/BalanceChangedListener.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using Boo.Lang;
-using NSubstitute.Core;
 using UE.Events;
 using UnityEngine;
 using UnityEngine.Events;
