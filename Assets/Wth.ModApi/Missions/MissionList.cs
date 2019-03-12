ce33e0b899396e1c4ddcd54ae564cb0931111279
Tobias-Melzer <tobias.melzer@study.hs-duesseldorf.de>
2019-03-12 18:36:07 +0100

    Mission Factory only generates Mission based on difficulty

--- Assets/Wth.ModApi/Missions/MissionList.cs
+++ Assets/Wth.ModApi/Missions/MissionList.cs
@@ -25,7 +25,7 @@ public class MissionList : ScriptableObject
     public string normalDifficultyDescription = "In this difficulty you can expect everything from the previous " +
                                                 "difficulty, as well as special missions on the topic of SQL-Injection.";
     public string hardDifficultyDescription = "In this difficulty you can expect everything from the previous " +
-                                              "difficulties, as well as special missions on the topic of CrossSiteScripting. ";
+                                              "difficulties, as well as special missions on the topic of CrossSiteScripting.";
     public string guruDifficultyDescription = "In this difficulty you can expect everything from the previous " +
                                               "difficulties, as well as special missions on the topic of Secure Passwords.";
 }
