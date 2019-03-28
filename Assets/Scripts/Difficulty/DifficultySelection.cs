df0c02711c95f0ac53c2c835805d68f975f68352
Tobias-Melzer <tobias.melzer@study.hs-duesseldorf.de>
2019-03-28 18:16:41 +0100

    Fixed Bug #90

---
+++ Assets/Scripts/Difficulty/DifficultySelection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameSystem;
+using UI;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class DifficultySelection : MonoBehaviour
+{
+    public GameObject DifficultyTitleText;
+    public GameObject DifficultyText;
+    public Dropdown Dropdown;
+    public MissionList DefaultMissionList;
+
+    private UnityAction<int> dropdownAction;
+    void Awake()
+    {
+        dropdownAction += ChangeDescription;
+        Dropdown.onValueChanged.AddListener(dropdownAction);
+        ChangeDescription(0);
+    }
+
+    private void ChangeDescription(int difficulty)
+    {
+        var diff = (SettingsManager.Difficulty) difficulty;
+        var missionList = ModHolder.Instance.GetMissionList() ?? DefaultMissionList;
+        string description = "";
+        string title = diff.ToString();
+        switch (diff)
+        {
+            case SettingsManager.Difficulty.Easy:
+                description = missionList.easyDifficultyDescription;
+                break;
+            case SettingsManager.Difficulty.Normal:
+                description = missionList.normalDifficultyDescription;
+                break;
+            case SettingsManager.Difficulty.Hard:
+                description = missionList.hardDifficultyDescription;
+                break;
+            case SettingsManager.Difficulty.Guru:
+                description = missionList.guruDifficultyDescription;
+                break;
+        }
+
+        DifficultyTitleText.GetComponent<Text>().text = title;
+        DifficultyText.GetComponent<Text>().text = description;
+    }
+
+}
